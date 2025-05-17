ed4ff1fd72d24ae12c8d0befb53cf1303646d85f
王小泽 <wangxiaoze@petalmail.com>
2025-05-17 19:30:56 +0800

    build: 更新构建脚本
    - 将 build:sh 脚本从 chmod +x build.sh 修改为 sh ./scripts/base.sh
    - 将 preinstall 脚本从 chmod +x pull.sh 修改为 sh ./scripts/pull.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# 写一个变量
+
+Re_Install="true"
 # 是否需要install
 if test "$Re_Install" = "true" ; then
 	rm -rf node_modules pnpm-lock.yaml
 	pnpm install
 fi
+
 pnpm run build
 docker cp ./dist 1Panel-openresty-0yGn:/www/sites/playground.wangxiaoze.cn/index
 
